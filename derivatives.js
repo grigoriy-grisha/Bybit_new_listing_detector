faa376de608869042e59a036722b38c9fe9fe7f9
Gavrilov Grigoriy <lowww1337@gmail.com>
2024-06-07 22:19:19 +0300

    setup derivatives

---
+++ derivatives.js
@@ -0,0 +1,79 @@
+const {RestClientV5} = require('bybit-api');
+
+
+const client = new RestClientV5({
+    key: process.env.KEY,
+    secret: process.env.SECRET
+});
+
+function findCommonElementsInArray(arr1, arr2) {
+    return arr2.filter(item1 => !arr1.find(item2 => item2.symbol === item1.symbol));
+}
+
+let list = [];
+
+const wait = (ms) => new Promise((r => setTimeout(r, ms)));
+
+
+async function handleTrade({symbol, qty}) {
+    const resultBuy = await client.submitOrder({
+            symbol,
+            qty,
+            category: "linear",
+            side: 'Buy',
+            orderType: "Market",
+            positionIdx: 1
+        }
+    )
+
+    console.log("_____BUY_DERIVATIVES_____")
+    console.log(resultBuy)
+
+    await wait(10000)
+
+    const resultSell = await client.submitOrder({
+            symbol,
+            qty,
+            category: "linear",
+            side: 'Sell',
+            orderType: "Market",
+            positionIdx: 1
+        }
+    )
+
+    console.log("_____SELL_DERIVATIVES_____")
+    console.log(resultSell)
+
+}
+
+async function runDerivatives() {
+    console.log("___START_DERIVATIVES___")
+    while (true) {
+        const response = await client
+            .getInstrumentsInfo({
+                category: 'linear',
+            })
+
+        if (list.length === 0) {
+            list = response.result.list
+            continue
+        }
+
+        const values = findCommonElementsInArray(list, response.result.list)
+        if (values.length !== 0) {
+
+            for (let i = 0; i < values.length; i++) {
+                console.log("____FOUND_NEW_PAIR_DERIVATIVES____")
+                console.log(values)
+                console.log("____FOUND_NEW_PAIR_DERIVATIVES_DATE____")
+                console.log(new Date())
+                console.log("____START_TRADING_DERIVATIVES____")
+                await handleTrade({symbol: values[i].symbol, qty: "30"})
+            }
+        }
+        list = response.result.list
+        await wait(80000)
+    }
+}
+
+module.exports = {runDerivatives}
---
+++ spot.js
@@ -0,0 +1,91 @@
+const {RestClientV5} = require('bybit-api');
+
+
+const client = new RestClientV5({
+    key: process.env.KEY,
+    secret: process.env.SECRET
+});
+
+function findCommonElementsInArray(arr1, arr2) {
+    return arr2.filter(item1 => !arr1.find(item2 => item2.symbol === item1.symbol));
+}
+
+let list = [];
+
+const wait = (ms) => new Promise((r => setTimeout(r, ms)));
+
+async function handleTrade({symbol, qty}) {
+    const resultBuy = await client.submitOrder({
+            symbol,
+            qty,
+            category: "spot",
+            side: 'Buy',
+            orderType: "Market"
+        }
+    )
+
+    console.log("_____BUY_SPOT_____")
+    console.log(resultBuy)
+
+    await wait(10000)
+
+    const balance = await getBalanceCoin(symbol);
+
+    console.log(balance)
+
+    const resultSell = await client.submitOrder({
+            symbol,
+            qty: Number(balance.equity).toFixed(2),
+            category: "spot",
+            side: 'Sell',
+            orderType: "Market"
+        }
+    )
+
+    console.log("_____SELL_SPOT_____")
+    console.log(resultSell)
+
+}
+
+async function getBalanceCoin(symbol) {
+    const balance = await client.getWalletBalance(
+        {
+            accountType: 'UNIFIED'
+        }
+    )
+
+    const wallet = balance.result.list[0]
+    const baseCoin = symbol.replace("USDT", "")
+
+    return wallet.coin.find(({coin}) => coin === baseCoin)
+}
+
+
+async function runSpot() {
+    console.log("___START_SPOT___")
+    while (true) {
+        const response = await client
+            .getInstrumentsInfo({
+                category: 'spot',
+            })
+
+        if (list.length === 0) {
+            list = response.result.list
+            continue
+        }
+
+        const values = findCommonElementsInArray(list, response.result.list)
+        if (values.length !== 0) {
+            console.log("____FOUND_NEW_PAIR_SPOT____")
+            console.log(values)
+            console.log("____FOUND_NEW_PAIR_SPOT_DATE____")
+            console.log(new Date())
+            console.log("____START_TRADING_SPOT____")
+            await handleTrade({symbol: values[0].symbol, qty: "30"})
+        }
+        list = response.result.list
+        await wait(500)
+    }
+}
+
+module.exports = {runSpot}
